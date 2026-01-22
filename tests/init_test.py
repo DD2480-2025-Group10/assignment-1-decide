7b6db06dc9dbbbd89f0d63ddfbf78d081b4fff65
Erik Söderlund <eriso@kth.se>
2026-01-22 11:47:39 +0100

    [TEST] Add test cases and small helper function to load test cases
    Added some cases for the whole program, i see now that the easiest way to do the smaller tests is to have the cases in the test file. The larger ones take up so much space though so they are nice outside.

--- tests/init_test.py
+++ tests/init_test.py
@@ -1,2 +1,34 @@
+import json
+from dataclasses import dataclass
+from typing import List, Dict, Any
+
+
+@dataclass
+class Data:
+    numpoints: int
+    x: List[float]
+    y: List[float]
+    parameters: Dict[str, Any]
+    lcm: Dict[str, Any]
+    puv: List[int]
+    expected_launch: bool
+
+
+def load_data(path: str) -> Data:
+    with open(path) as f:
+        data = json.load(f)
+
+    return Data(
+        numpoints=data["NUMPOINTS"],
+        x=data["X"],
+        y=data["Y"],
+        parameters=data["PARAMETERS"],
+        lcm=data["LCM"],
+        puv=data["PUV"],
+        expected_launch=bool(data["EXPECTED"]["LAUNCH"])
+    )
+
 def test_example():
-    assert True
+    d = (load_data("./whole_program_cases/case0_false.json"))
+    if d != None:
+      assert True
