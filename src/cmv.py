f4f5baf8bd7acffb4376290663718231196bd5ab
William Sassner Andersson <williamsassner@outlook.com>
2026-01-22 15:56:33 +0100

    [FEAT]: Implemented LIC0 function

--- src/cmv.py
+++ src/cmv.py
@@ -3,3 +3,15 @@ from src.utils import *
 This file will contain all 15 LIC functions
 
 """
+
+def LIC0() -> bool:
+
+    if NUMPOINTS < 2:
+        return False
+    
+    for i in range(NUMPOINTS - 1):
+        distance = calculate_distance(POINTS[i], POINTS[i + 1])
+        if double_compare(distance, PARAMETERS2.length1) == COMPTYPE.GT:
+            return True
+        
+        return False
