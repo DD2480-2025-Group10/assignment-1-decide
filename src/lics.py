bbd4e29c56258600d3fc914d9304e2174d41157d
Villiam riegler <ville@riegler.se>
2026-01-23 09:31:56 +0100

    [REFACTOR]: Set up consistent interfaces for LICS and CMV using those interfaces
    - Creates LIC interface
    - Creates CMV builder interface that implements LIC evaluation logic for
    CMV
    - Resolves issues regarding circular imports by creating a new types.py
    file
    - Moves some utils from utils.py and inline inside of cmv.py into a
    plane_utils.py file.
    
    SOLVES #68

---
+++ src/lics.py
@@ -0,0 +1,101 @@
+from dataclasses import dataclass
+from typing import Protocol
+
+from src.plane_utils import Quadrant, Ray, calculate_distance, vector_magnitude
+from src.types import Parameters_T, PointList
+
+
+# Protocol for LIC Rule classes
+# This defines the interface that all LIC rule implementations must follow.
+class LicRule(Protocol):
+    # Unique identifier for the launch interception condition,
+    # this is to make sure each rule is ordered correctly.
+    # :Use the indetifier from the respective LICS rule set (ex. LIC2 -> 2)
+    @property
+    def ident(self) -> int:
+        ...
+
+    # Evaluete function for the LIC rule. 
+    # :Param points: List of points to evaluate the rule on
+    # :Param params: Parameters_T object containing all parameters
+    def evaluate(self, points: PointList, params: Parameters_T) -> bool:
+        ...
+
+# **********************************
+# Conctete LIC Rule Implementations
+# **********************************
+
+
+@dataclass(frozen=True)
+class LIC4:
+    ident: int = 4
+
+    """
+    Evaluates LIC4: There exists at least one set of Q_PTS consecutive points
+    that lie in more than QUADS quadrants. That is, Q_PTS points in a row that
+    lie in separate quartants, such that the number of quartants is greater
+    than QUADS.
+    """
+    def evaluate(self, points: PointList, params: Parameters_T) -> bool:
+        quadrants = [Quadrant.from_point(point) for point in points]
+
+        for i in range(len(quadrants) - params.q_pts + 1):
+            unique_quadrants = set(quadrants[i:i + params.q_pts])
+
+            if len(unique_quadrants) > params.quads:
+                return True
+
+        return False
+
+@dataclass(frozen=True)
+class LIC6:
+    ident: int = 6
+
+    """
+    Evaluates LIC6: There exists at least one set of N_PTS consecutive points
+    such that at least one intermediate point is a distance greater than dist
+    from the line segment joining the first and last points of the set.
+    """
+
+    def evaluate(self, points: PointList, params: Parameters_T) -> bool:
+        if len(points) < 3:
+            return False
+
+        for i in range(len(points) - params.n_pts + 1):
+            point_slice = points[i:i + params.n_pts]
+            A = point_slice[0]
+            B = point_slice[-1]
+
+            # Create Ray from A to B
+            ray = Ray(A, B)
+
+            # Evalueate if any intermediate point is at a distance greate than dist from the ray
+            for point in point_slice[1:-1]:
+                proj_point = ray.project_point(point)
+                vec_diff = (point[0] - proj_point[0], point[1] - proj_point[1])
+                if vector_magnitude(vec_diff) > params.dist:
+                    return True
+
+        return False
+
+@dataclass(frozen=True)
+class LIC7:
+    ident: int = 7
+
+    """
+    Evaluates LIC7: There exists at least one set of K_PTS consecutive points
+    such that the distance between the first and last point is greater than LENGTH1.
+    """
+
+    def evaluate(self, points: PointList, params: Parameters_T) -> bool:
+        if len(points) < 3:
+            return False
+
+        for i in range(len(points) - params.k_pts - 1):
+            A = points[i]
+            B = points[i + params.k_pts + 1]
+
+            if calculate_distance(A, B) > params.length1:
+                return True
+
+        return False
