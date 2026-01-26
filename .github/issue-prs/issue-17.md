4e5a8a4c01d8362f20f85deed7fc7d7da374823a
Erik Söderlund <eriso@kth.se>
2026-01-26 11:36:23 +0100

    [Issue #17] [FEAT]: (LIC 5) Consecutive X-coordinate check (#17)* [create-pull-request] automated change
    * [FEAT]: Implement LIC5 function
    
    Implements the functionality of LIC5. Evaluates whether there exists
    at least one set of two consecutive data points (X[i], Y[i]) and
    (X[j], Y[j]) such that X[j] - X[i] < 0 where j = i + 1.
    
    * [TEST]: Add tests for implementation of LIC5
    
    - Added tests/lic5_test.py to verify positive and negative cases.
    - Confirmed Y-coordinates do not affect the logic.
    
    * [CHORE]: Register LIC5 in DefaultCmvBuilder
    
    - Updated src/cmv.py to register LIC5 in the builder singleton.
    
    ---------
    
    Co-authored-by: erik <erik@n153-p24.assignment1soffan>

---
+++ .github/issue-prs/issue-17.md
@@ -0,0 +1,1 @@
+# Work for issue #17: [FEAT]: (LIC 5) Consecutive ahead
--- src/cmv.py
+++ src/cmv.py
@@ -11,6 +11,7 @@ from src.lics import (
     LIC6,
     LIC7,
     LIC8,
+    LIC5,
     LicRule,
 )
 from src.types import Parameters_T, PointList
@@ -58,7 +59,7 @@ DefaultCmvBuilder = (
     .register_lic(LIC2())
     .register_lic(LIC3())
     .register_lic(LIC4())
-    # Add LIC5 later
+    .register_lic(LIC5()) 
     .register_lic(LIC6())
     .register_lic(LIC7())
     .register_lic(LIC8())
